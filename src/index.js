e0ae8b206468ecb3018ae004b435dbf0d35d18ca
Daniel Bueno <danibuenofernandez@gmail.com>
2024-09-28 19:18:09 +0200

    import type

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './Home';
+import Home from './Home.js';
 
 import reportWebVitals from './reportWebVitals';
 

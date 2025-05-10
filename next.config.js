476ce1a39ae29e3c5fe8e0b7506606ac6426b144
Daniel Bueno Fernández <danielbuenofernandez@MacBook-Air-de-Dan.local>
2025-05-10 21:12:16 +0200

    Fix: Simplificar configuración al mínimo para resolver problemas de despliegue

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  output: 'standalone',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
-  trailingSlash: true
+  reactStrictMode: true
 };
 
 module.exports = nextConfig; 

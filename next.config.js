e4a94f8975958af0d90fb8df30f358d9558c83c6
Daniel Bueno Fernández <danielbuenofernandez@MacBook-Air-de-Dan.local>
2025-05-10 21:48:20 +0200

    Clean: Eliminar configuración de Netlify para reiniciar desde cero

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  }
+  reactStrictMode: true
 };
 
 module.exports = nextConfig; 

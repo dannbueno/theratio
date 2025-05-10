27620ffc3c56b01684294038f9c83e2b1077f15f
Daniel Bueno Fernández <danielbuenofernandez@MacBook-Air-de-Dan.local>
2025-05-10 21:04:00 +0200

    Fix: Configurar para exportación estática para resolver error 404

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone'
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true
 };
 
 module.exports = nextConfig; 

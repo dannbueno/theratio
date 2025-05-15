55ba04c7f276169087967431757520e02f65d358
Dan Bueno <danibuenofernandez@gmail.com>
2025-05-15 09:26:35 +0200

    Configure next.js for static export

--- next.config.js
+++ next.config.js
@@ -1,21 +1,14 @@
 /** @type {import('next').NextConfig} */
-const path = require('path');
-
-const nextConfig = {
-  reactStrictMode: true,
-  transpilePackages: ['react-leaflet', 'leaflet'],
-  webpack(config) {
-    config.resolve.alias['@'] = path.join(__dirname, '');
-    return config;
-  },
-  // Ignorar errores de ESLint durante la compilación
+module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  // Ignorar errores de TypeScript durante la compilación
   typescript: {
     ignoreBuildErrors: true,
+  },
+  output: 'export',
+  distDir: 'out',
+  images: {
+    unoptimized: true
   }
-};
-
-module.exports = nextConfig; 
+} 
